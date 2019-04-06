696bdf30f40bc2e3845ff5ed4546d763f06ea512
maoshuchen(陈津) <maoshuchen@tencent.com>
2019-04-06 17:44:16 +0800

    feat: version 1.0.0

---
+++ README.md
@@ -0,0 +1,142 @@
+# Express View Cache Clean
+
+Express enables view template compilation caching in production (when `NODE_ENV` is `production`) which can improve performance. `express-view-cache-clean` can watch view file changes to clean relative caches.
+
+## Installation
+
+```shell
+yarn add express-view-cache-clean
+```
+
+or
+
+```shell
+npm i express-view-cache-clean --save
+```
+
+## Example
+
+```js
+const express = require('express')
+const expressViewCacheClean = require('express-view-cache-clean')
+
+const app = express()
+
+expressViewCacheClean({
+  app,
+  viewPath: './views',
+  // when filename includes 'page/home' view file changes, it will clean name includes 'page/home' cache
+  rules: ['page/home', 'page/category'],
+  recursive: true
+})
+
+```
+
+## Options
+
+**Clean Up Options**
+
+* `app: Express` (required)
+* `viewPath: String` (required)
+* `rules: Object | Array<String> | Array<Object>` (required)
+
+   Multiple `rules` configuration formats:
+
+   ```js
+   // when filename includes 'page' view file changes, it will clean name includes 'page/home' cache
+   rules: {
+     'page/home': 'page/home',
+   }
+
+   rules: [
+     'page/home',
+     'page/category'
+   ]
+
+   rules: [{
+     filename: 'page/home',
+     cache: 'page/home'
+   }]
+   ```
+
+   Multiple `filename` configuration formats:
+
+   ```js
+   // String
+   filename: 'page/home'
+
+   // Function
+   filename: (filename) => {
+     return filename.includes('page/home')
+   }
+
+   // Regex
+   filename: /page\/home/
+   ```
+
+   Multiple `cache` configuration formats:
+
+   ```js
+   // String
+   cache: 'page/home'
+
+   // Function
+   cache: (view, filename, app) => {
+     return view.name.includes('page/home')
+   }
+
+   // RegExp
+   cache: /page\/home/
+
+   // Array<String | Function | RegExp>
+   cache: [
+     'page/home',
+     (view, filename, app) => {
+       return view.name.includes('page/second')
+     },
+     cache: /page\/other/
+   ]
+   ```
+
+
+**Watch Options**
+
+The usage and options are compatible with [fs.watch](https://nodejs.org/dist/latest-v7.x/docs/api/fs.html#fs_fs_watch_filename_options_listener).
+* `persistent: Boolean` (default **true**)
+* `recursive: Boolean` (default **false**)
+* `encoding: String` (default **'utf8'**)
+* `filter: RegExp | Function` (default **null**)
+
+   Return that matches the filter expression.
+
+    ```js
+    // filter with regular expression
+    filter: /\.json$/
+
+    // filter with custom function
+    filter: f => !/node_modules/.test(f)
+    ```
+
+* `delay: Number` (in ms, default **200**)
+
+   Delay time of the callback function.
+
+   ```js
+   // log after 5 seconds
+   delay: 5000
+   ```
+
+* `events: Array<String>` (default **['update']**)
+
+   Watch file events, `update` or `remove`.
+
+   ```js
+   // only watch file update event
+   events: ['update']
+   // watch file all event
+   events: ['update', 'remove']
+   ```
+
+## License
+
+[MIT](./LICENSE)
---
+++ index.js
@@ -0,0 +1,139 @@
+const watch = require('node-watch')
+
+const defaultOptions = {
+  app: undefined,
+  persistent: true,
+  recursive: false,
+  encoding: 'utf8',
+  filter: undefined,
+  delay: 200,
+  viewPath: undefined,
+  events: ['update'] // ['update', 'remove']
+}
+
+const checkExist = (options, key) => {
+  const ext = key ? `.${key}` : ''
+  const obj = key ? options[key] : options
+  if (!obj) {
+    throw new Error(`expressViewCacheClean options${ext} can not be null!`)
+  }
+}
+
+const convertToArrayRules = (objRules) => Object.entries(objRules).map(([k, v]) => ({
+  filename: k,
+  cache: v
+}))
+
+const stringRuleMapper = (r) => {
+  if (typeof r !== 'string') {
+    return r
+  }
+
+  return {
+    filename: r,
+    cache: r
+  }
+}
+
+const matchRule = (filename) => (r) => {
+  const type = typeof r.filename
+  if (type === 'string') {
+    if (r.filename === '*') {
+      return true
+    }
+    return filename.includes(r.filename)
+  }
+  if (type === 'function') {
+    return r.filename(filename)
+  }
+  if (r.filename instanceof RegExp) {
+    return r.filename.test(filename)
+  }
+  return false
+}
+
+const removeCache = (filename, app) => (r) => {
+  if (!r.cache) {
+    throw new Error(`rule '${r.filename}', cache can not set empty.`)
+  }
+
+  if (!app.cache) {
+    return
+  }
+
+  let caches
+  if (!Array.isArray(r.cache)) {
+    caches = [r.cache]
+  } else {
+    caches = r.cache
+  }
+
+  Object.entries(app.cache).forEach(([name, view]) => {
+    const key = view.path || name || ''
+    for (let i = 0; i < caches.length; i++) {
+      const h = caches[i]
+      if (typeof h === 'string') {
+        if (h === '*' || key.includes(h)) {
+          delete app.cache[name]
+          break
+        }
+      } else if (typeof h === 'function') {
+        if (h(view, filename, app)) {
+          delete app.cache[name]
+          break
+        }
+      } else if (h instanceof RegExp) {
+        if (h.test(key)) {
+          delete app.cache[name]
+          break
+        }
+      }
+    }
+  })
+}
+
+const getCleaner = (options) => {
+  let rules
+  if (!Array.isArray(options.rules)) {
+    rules = convertToArrayRules(options.rules)
+  } else {
+    rules = options.rules
+  }
+
+  return (filename) => {
+    if (!filename) {
+      return
+    }
+
+    rules
+      .map(stringRuleMapper)
+      .filter(matchRule(filename))
+      .forEach(removeCache(filename, options.app))
+  }
+}
+
+const expressViewCacheClean = (options) => {
+  options = Object.assign({}, defaultOptions, options)
+
+  checkExist(options)
+  checkExist(options.app)
+  checkExist(options.viewPath)
+  checkExist(options.events)
+  checkExist(options.rules)
+
+  const cleaner = getCleaner(options)
+
+  return watch(options.viewPath, {
+    persistent: options.persistent,
+    recursive: options.recursive,
+    encoding: options.encoding,
+    filter: options.filter,
+    delay: options.delay
+  }, (evt, name) => {
+    if (options.events.includes(evt)) {
+      cleaner(name)
+    }
+  })
+}
+
+module.exports = expressViewCacheClean
